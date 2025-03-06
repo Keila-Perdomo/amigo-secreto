0d2c6201130bf157529081566f9108d8c4f7d65a
Keila-Perdomo <keila.phe@gmail.com>
2025-03-05 22:45:38 -0600

    crear el index.html

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+    let lista = document.getElementById("listaAmigos");
+
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+    }
+    
+    if (amigos.includes(nombre)) {
+        alert("Este nombre ya ha sido agregado.");
+        return;
+    }
+    
+    amigos.push(nombre);
+    
+    let listItem = document.createElement("li");
+    listItem.textContent = nombre;
+    lista.appendChild(listItem);
+    
+    input.value = ""; 
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Agrega al menos un amigo antes de realizar el sorteo.");
+        return;
+    }
+    
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSecreto = amigos[indiceAleatorio];
+    
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>🎉 El amigo secreto es: <strong>${amigoSecreto}</strong> 🎉</li>`;
+}
 
